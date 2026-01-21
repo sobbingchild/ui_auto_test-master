2e0974d72ab29f5666ef31937779d3e1024a2041
sobbingchild <66291315+sobbingchild@users.noreply.github.com>
2026-01-21 17:37:35 +0800

    Update base_info.py
    test0121

--- ui_auto_test/bk_cmdb/base_info.py
+++ ui_auto_test/bk_cmdb/base_info.py
@@ -17,21 +17,3 @@ class BaseInfomation():
         except:
             print("配置文件没有找到{}:{}字段".format(sessiondata, key))
 
-    # 读取config配置文件中指定数据
-    def read_config_all(self, sessiondata, path):
-        config = ConfigObj(path, encoding='UTF-8')
-        try:
-            value = config[sessiondata]
-            return value
-        except:
-            print("配置文件没有找到{}".format(sessiondata))
-
-    # 写对应数据到config配置文件
-    def write_config(self, key, value, path):
-        config = ConfigObj(path, encoding='UTF-8')
-        # config.read(self.config_path)
-        # 判断参数是否为int
-        if "int" in key:
-            value = str(value)
-        config["testdata"][key] = value
-        config.write()
